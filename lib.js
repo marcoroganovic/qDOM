b93fdbeb3d9f6cc5976f84e63207290c2d72e5eb
Marco Roganovic <marcoroganovic@gmail.com>
2017-01-24 20:20:44 +0100

    start over

--- lib.js
+++ lib.js
@@ -1,45 +1,12 @@
 (function(global) {
- 
-  // small templating
-  function parseHTML(template, data) {
-    var partOne = "{{\\s*?";
-    var partTwo = "\\s*?}}";
-    var re;
 
-    for(var prop of data) {
-      re = new RegExp(partOne +  prop + partTwo, "g");
-      template = template.replace(re, data[prop]);
+  function dom(sel) {
+    if(!(sel instanceof this)) {
+      return new dom(sel);
     }
 
-    return template;
+    var elements = document.querySelectorAll(sel);
+    Array.prototype.push.call(this, elements);
   }
-
-  // event delegation
-  function delegate(obj) {
-    obj.el.addEventListener(obj.type, function(e) {
-      e.preventDefault();
-      var nodeName = e.target.nodeName.toLowerCase();
-
-      if(nodeName === obj.to.toLowerCase()) {
-        obj.callback.call(e.target, e);
-      }
-    });
-  }
-
-  // delegate({
-    // el: $list,
-    // type: "click",
-    // to: "li",
-    // callback: function(e) {
-    //   console.log(this);
-    // }
-  // });
-
-  var utils = {
-    parseHTML: parseHTML,
-    delegate: delegate
-  }
-
-  global.dom =  utils
 
 })(this);
