f732a5bcfc79d25ed644645254c14dd53d0783cc
Marco Roganovic <marcoroganovic@gmail.com>
2017-01-25 19:08:26 +0100

    make clear what dom function does

--- lib.js
+++ lib.js
@@ -1,12 +1,20 @@
 (function(global) {
 
+  // it returns an object with DOM elements (if any) and length property
   function dom(sel) {
-    if(!(sel instanceof this)) {
+    if(!(this instanceof dom)) {
       return new dom(sel);
     }
 
-    var elements = document.querySelectorAll(sel);
-    Array.prototype.push.call(this, elements);
+    var nodes = Array.from(document.querySelectorAll(sel));
+    nodes.forEach(function(node, i, arr) {
+      this[i] = node;
+    }.bind(this));
+
+    this.length = nodes.length;
   }
+
+
+  global.dom = dom;
 
 })(this);
